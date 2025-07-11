aab0c270394ae4a2925627132b1c5e5486e21958
Sandra Pavan <sandrapavan38@gmail.com>
2025-07-10 21:19:23 -0300

    :hammer: incluindo os arquivos para configuracao do grafana e openTelemetry

---
+++ app/src/main.ts
@@ -0,0 +1,15 @@
+import { sdk } from './tracer'
+sdk.start();
+import { log } from './infra/logger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001).then(() => {
+    log.info('Aplicação subiu!!');
+  }).catch(() => {
+    log.error('Aplicação não subiu!!')
+  });
+}
+bootstrap();
